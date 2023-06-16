b5abaf5ea5d2c73cb0dbce4c58eb51cb279d6c7b
cassmello <abdulkadirimahamadu@gmail.com>
2023-06-15 20:06:29 -0700

    task 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+* main - Write a C program that prints exactly.
+* Return: 0 (Success)
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
