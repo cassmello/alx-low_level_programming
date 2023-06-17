37d4d0ef9dfe601b7542191a77a9fd9d633a9e38
cassmello <abdulkadirimahamadu@gmail.com>
2023-06-16 19:42:26 -0700

    task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - input code
+ * Return: 0 (Achieve)
+ */
+
+int main(void)
+{
+	char c;
+	while
+		(c <= 'z') {
+			putchar(c);
+			c++;
+		}
+	putchar('\n');
+
+	return (0);
+}
---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+ * main - input code 
+ * Return: 0 (achieve)
+ */
+
+int main(void)
+{
+	char c;
+	char b;
+
+	c = 'a';
+	b = 'A';
+	while
+		(c <= 'z') {
+			putchar(c);
+			c++;
+		}
+	while
+		(b <= 'Z') {
+			putchar(b);
+			b++;
+		}
+	putchar('\n');
+
+	return (0);
+}
