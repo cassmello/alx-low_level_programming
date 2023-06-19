489821ab2cf9b6f237ab6d099615915ad68e5232
cassmello <abdulkadirimahamadu@gmail.com>
2023-06-18 17:54:51 -0700

    task 7

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,13 +6,14 @@
 
 
 int main(void)
+/*for betty documentation*/
 {
 printf("Size of char: %lu byte\n", sizeof(char));
 printf("Size of int: %lu bytes\n", sizeof(int));
 printf("Size of long int: %lu bytes\n", sizeof(long int));
 printf("Size of long long int : %lu bytes\n", sizeof(long long int));
 printf("Size of float: %lu bytes\n", sizeof(float));
-
+/*for betty documentation*/
 return (0);
 }
 
