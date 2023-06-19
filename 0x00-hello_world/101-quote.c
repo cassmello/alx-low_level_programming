a12d2ebc89e196aa15672921c53091568b2474a2
cassmello <abdulkadirimahamadu@gmail.com>
2023-06-18 19:19:51 -0700

    fg

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -5,21 +5,23 @@
 #define STDERR_FILENO 2
 /**
  * main - Write a C program that print
- * return:0 (achived)
+ * return:1 (achived)
+ * write_str_to_stderr:write a string
 */
 
-void write_str_to_stderr(const char *str) {
-    size_t len = 0;
-    while (str[len] != '\0')
-        len++;
+void write_str_to_stderr(const char *str)
+{
+size_t len = 0;
+while (str[len] != '\0')
+len++;
 
-    syscall(SYS_write, STDERR_FILENO, str, len);
+syscall(SYS_write, STDERR_FILENO, str, len);
 }
 
 int main(void)
 {
-    const char *message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-    write_str_to_stderr(message);
-    return 1;
+const char *message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+write_str_to_stderr(message);
+return (1);
 }
 
