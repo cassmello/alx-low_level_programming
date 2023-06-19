767b6688848f108b3f32f602ae4693313c13737e
cassmello <abdulkadirimahamadu@gmail.com>
2023-06-18 18:22:45 -0700

    6666

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -11,7 +11,7 @@ int main(void)
 /*for betty documentation*/
 printf("Size of a char: 1 byte(s)\n");
 printf("Size of an int: 4 byte(s)\n");
-printf("Size of a long int: 4 byte(s)\n");
+printf("Size of a long int: %lu byte(s)\n", sizeof(long int));
 printf("Size of a long long int: 8 byte(s)\n");
 printf("Size of a float: 4 byte(s)\n");
 /*for betty documentation*/
