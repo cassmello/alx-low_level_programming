181bf0de4cb7cf81b3593f2885ab4aaafd9b1344
cassmello <abdulkadirimahamadu@gmail.com>
2023-06-26 22:57:35 -0700

    task 100

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -9,39 +9,36 @@
 
 int _atoi(char *s)
 {
-	int a, b, c, len, d, e;
+    int a, c, len, d, e;
 
-	a = 0;
-	b = 0;
-	c = 0;
-	len = 0;
-	d = 0;
-	e = 0;
+    a = 0;
+    c = 0;
+    len = 0;
+    d = 0;
+    e = 0;
 
-	while (s[len] != '\0')
-		len++;
+    while (s[len] != '\0')
+        len++;
 
-	while (a < len && d == 0)
-	{
-		if (s[a] == '-')
-			++b;
+    while (a < len && d == 0)
+    {
+        if (s[a] == '-' && d == 0)
+            b = -1;
 
-		if (s[a] >= '0' && s[a] <= '9')
-		{
-			e = s[a] - '0';
-			if (d % 2)
-				e = -e;
-			c = c * 10 + e;
-			d = 1;
-			if (s[a + 1] < '0' || s[a + 1] > '9')
-				break;
-			d = 0;
-		}
-		a++;
-	}
+        if (s[a] >= '0' && s[a] <= '9')
+        {
+            e = s[a] - '0';
+            if (d % 2)
+                e = -e;
+            c = c * 10 + e;
+            d = 1;
+            if (s[a + 1] < '0' || s[a + 1] > '9')
+                break;
+            d = 0;
+        }
+        a++;
+    }
 
-	if (d == 0)
-		return (0);
-
-	return (c);
+    return c;
 }
+
