55ad7906cf0e3137b7c667d90220e39d2c5b336a
cassmello <abdulkadirimahamadu@gmail.com>
2023-07-11 19:44:41 -0700

    0-strcat

--- 0x09-static_libraries/0-strcat.c
+++ 0x09-static_libraries/0-strcat.c
@@ -9,27 +9,25 @@
 
 char *_strcat(char *dest, char *src)
 {
-        	int x;
-        	int y;
+	int x;
+	int y;
 
-        	x = 0;
+	x = 0;
 
-        	while (dest[x] != '\0')
-        	{
-                    	x++;
-        	}
+	while (dest[x] != '\0')
+	{
+		x++;
+	}
 
-        	y = 0;
+	y = 0;
 
-        	while (src[y] != '\0')
-        	{
-                    	dest[x] = src[y];
-                    	x++;
-                    	y++;
-        	}
+	while (src[y] != '\0')
+	{
+		dest[x] = src[y];
+		x++;
+		y++;
+	}
 
-        	dest[x] = '\0';
-        	return (dest);
-
-
+	dest[x] = '\0';
+	return (dest);
 }
