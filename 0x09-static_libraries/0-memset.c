066d036cc8ae68744c2002bc138fa34c29b8e122
cassmello <abdulkadirimahamadu@gmail.com>
2023-07-11 19:31:29 -0700

    0-memset

--- 0x09-static_libraries/0-memset.c
+++ 0x09-static_libraries/0-memset.c
@@ -10,13 +10,13 @@
 
 char *_memset(char *s, char b, unsigned int n)
 {
-   	int x = 0;
+	int x = 0;
 
-   	for (; n > 0; x++)
-   	{
-          	s[x] = b;
-          	n--;
-   	}
+	for (; n > 0; x++)
+	{
+		s[x] = b;
+		n--;
+	}
 
-   	return (s);
+	return (s);
 }
