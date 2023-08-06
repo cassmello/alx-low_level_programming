a77bcf2d5dfca73b5dd75028007362f9b1e588b6
cassmello <abdulkadirimahamadu@gmail.com>
2023-08-05 19:36:29 -0700

    unix

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,27 +1,13 @@
-#include <stdio.h>
 #include <unistd.h>
-#include <sys/syscall.h>
-
-#define STDERR_FILENO 2
-/**
- * main - Write a C program that print
- * return:1 (achived)
- * write_str_to_stderr:write a string
-*/
-
-void write_str_to_stderr(const char *str)
-{
-size_t len = 0;
-while (str[len] != '\0')
-len++;
-
-syscall(SYS_write, STDERR_FILENO, str, len);
-}
 
 int main(void)
 {
-const char *message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-write_str_to_stderr(message);
-return (1);
+    char unix[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+    ssize_t len = 59;
+
+    write(2, unix, len);
+
+    return (1);
 }
+
 
